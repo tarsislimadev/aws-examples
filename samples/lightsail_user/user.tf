c219617c8dd86fa4559da18f4f2c4d417b01f6ae
Tarsis de Lima <br.tmvdl@gmail.com>
2022-09-26 14:11:29 -0300

    Fixing user creation in Terraform file

--- samples/lightsail_user/user.tf
+++ samples/lightsail_user/user.tf
@@ -18,22 +18,6 @@ resource "aws_iam_user" "iam_user" {
   name = "user-${var.user_name}"
 }
 
-# resource "aws_iam_role" "iam_role" {
-#   name = "role-${var.user_name}"
-
-#   assume_role_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Action" : "lightsail:*",
-#         "Effect" : "Allow",
-#         "Resource" : "*"
-#       }
-#     ]
-#   })
-
-# }
-
 resource "aws_iam_group" "iam_group" {
   name = "iam_group-${var.user_name}"
 }
@@ -58,8 +42,6 @@ resource "aws_iam_policy_attachment" "iam_policy_attachment" {
   name = "aws_iam_policy_attachment-${var.user_name}"
 
   users = [aws_iam_user.iam_user.name]
-
-  # roles = [aws_iam_role.iam_role.name]
 
   groups = [aws_iam_group.iam_group.name]
 
